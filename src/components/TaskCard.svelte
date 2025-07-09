861b1d605000f08034cd29905ac8e9cf4a91a16a
verdulive <verdu@live.com>
2025-07-09 14:14:35 +0200

    test timeout format

--- src/components/TaskCard.svelte
+++ src/components/TaskCard.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { formatTime } from '@/lib/utils';
 	import Avatar from '@/components/Avatar.svelte';
 
 	let { task } = $props();
@@ -7,12 +8,12 @@
 <article class="flex gap-1 rounded-lg bg-gray-800 p-2 transition-colors hover:bg-gray-700">
 	<Avatar owner={task.owner} />
 
-	<div class="flex flex-col">
+	<div class="flex w-full flex-col">
 		<h3>{task.description}</h3>
 
-		<footer class="flex items-center justify-between">
+		<footer class="flex w-full items-center justify-between">
 			<p>{task.client}</p>
-			<p>{task.timeout}</p>
+			<p>{formatTime(task.timeout)}</p>
 		</footer>
 	</div>
 </article>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,1 +1,3 @@
-//
+export function formatTime(timeout: number) {
+  return new Intl.DurationFormat("es-ES", { minutes: "narrow", seconds: "narrow" }).format({ minutes: timeout / 60 });
+}
